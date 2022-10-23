20dfbcda2631ec60f1e9e873a8eda21c56787561
aoermolenko <a.o.ermolenko@gmail.com>
2022-10-23 21:17:34 +0300

    add dhtmlx kanban plugin

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -5,29 +5,10 @@
 @endsection
 
 @section('content')
-    <style>
-        .kanban-col {
-            padding: 0px 3px 0px 3px;
-            width: 270px;
-        }
-        .kanban-col .card-body {
-            padding: 5px 5px;
-        }
-        .card .draggable {
-            margin-bottom: 10px;
-            cursor: grab;
-            height: 65px;
-            border: 0px;
-        }
-        .droppable {
-            background-color: #0f5132;
-            min-height: 120px;
-            margin-bottom: 1rem;
-        }
-        .dropzone {
-            height: 7px;
-        }
-    </style>
+    <link href="{{ asset('assets/css/kanban/demos.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/kanban/kanban.css') }}" rel="stylesheet">
+    <script src="{{ asset('assets/js/kanban/kanban.js') }}"></script>
+
     <div class="container-fluid px-4">
         <h1 class="mt-4">Tasks</h1>
         <ol class="breadcrumb mb-4">
@@ -38,191 +19,243 @@
             <p>kanban tasks TODO</p>
         </div>
         <div class="kanban-board row flex-row flex-sm-nowrap py-3">
-            <div class="kanban-col col-sm-6 col-md-4 col-xl-3">
-                <div class="card bg-light">
-                    <div class="card-header">
-                        <h6 class="card-title text-uppercase text-truncate py-2">To Do</h6>
-                    </div>
-                    <div class="card-body">
-
-                        <div class="items border border-light">
-                            <div class="dropzone rounded" ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="clearDrop(event)"></div>
-                            <div class="card draggable shadow-sm" id="cd0" draggable="true" ondragstart="drag(event)">
-                                <div class="card-body p-2">
-                                    <p>
-                                        This is a description of a item on the board. 1
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="dropzone rounded" ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="clearDrop(event)"></div>
-                            <div class="card draggable shadow-sm" id="cd1" draggable="true" ondragstart="drag(event)">
-                                <div class="card-body p-2">
-                                    <p>
-                                        This is a description of a item on the board. 2
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="dropzone rounded" ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="clearDrop(event)"></div>
-                            <div class="card draggable shadow-sm" id="cd2" draggable="true" ondragstart="drag(event)">
-                                <div class="card-body p-2">
-                                    <p>
-                                        This is a description of a item on the board. 3
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="dropzone rounded" ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="clearDrop(event)"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="kanban-col col-sm-6 col-md-4 col-xl-3">
-                <div class="card bg-light">
-                    <div class="card-header">
-                        <h6 class="card-title text-uppercase text-truncate py-2">In-progess</h6>
-                    </div>
-                    <div class="card-body">
-                        <div class="items border border-light">
-                            <div class="dropzone rounded" ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="clearDrop(event)"></div>
-                            <div class="card draggable shadow-sm" id="cd3" draggable="true" ondragstart="drag(event)">
-                                <div class="card-body p-2">
-                                    <p>
-                                        This is a description of a item on the board. 4
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="dropzone rounded" ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="clearDrop(event)"></div>
-                            <div class="card draggable shadow-sm" id="cd4" draggable="true" ondragstart="drag(event)">
-                                <div class="card-body p-2">
-                                    <p>
-                                        This is a description of a item on the board. 5
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="dropzone rounded" ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="clearDrop(event)"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="kanban-col col-sm-6 col-md-4 col-xl-3">
-                <div class="card bg-light">
-                    <div class="card-header">
-                        <h6 class="card-title text-uppercase text-truncate py-2">Review</h6>
-                    </div>
-                    <div class="card-body">
-
-                        <div class="items border border-light">
-                            <div class="dropzone rounded" ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="clearDrop(event)"></div>
-                            <div class="card draggable shadow-sm" id="cd5" draggable="true" ondragstart="drag(event)">
-                                <div class="card-body p-2">
-                                    <p>
-                                        This is a description of a item on the board. 6
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="dropzone rounded" ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="clearDrop(event)"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="kanban-col col-sm-6 col-md-4 col-xl-3">
-                <div class="card bg-light">
-                    <div class="card-header">
-                        <h6 class="card-title text-uppercase text-truncate py-2">Complete</h6>
-                    </div>
-                    <div class="card-body">
-                        <div class="items border border-light">
-                            <div class="dropzone rounded" ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="clearDrop(event)"></div>
-                            <div class="card draggable shadow-sm" id="cd6" draggable="true" ondragstart="drag(event)">
-                                <div class="card-body p-2">
-                                    <p>
-                                        This is a description of a item on the board. 7
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="dropzone rounded" ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="clearDrop(event)"></div>
-                            <div class="card draggable shadow-sm" id="cd7" draggable="true" ondragstart="drag(event)">
-                                <div class="card-body p-2">
-                                    <p>
-                                        This is a description of a item on the board. 8
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="dropzone rounded" ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="clearDrop(event)"></div>
-                        </div>
-                    </div>
-                </div>
+            <div class="g-wrap">
+                <div id="toolbar"></div>
+                <div id="root"></div>
             </div>
         </div>
     </div>
     <script>
-        const drag = (event) => {
-            event.dataTransfer.setData("text/plain", event.target.id);
+        function getData() {
+            const users = [
+                {
+                    id: 1,
+                    label: "Steve Smith",
+                    avatar: "https://via.placeholder.com/30/0000FF",
+                    //avatar: "../assets/user.jpg",
+                },
+                {
+                    id: 2,
+                    label: "Aaron Long",
+                    avatar: "https://via.placeholder.com/30/FF0000",
+                },
+                {
+                    id: 3,
+                    label: "Angela Allen",
+                    avatar: "https://via.placeholder.com/30/00FF00",
+                },
+                {
+                    id: 4,
+                    label: "Angela Long",
+                    avatar: "https://via.placeholder.com/30/F0F0F0",
+                },
+            ];
+
+            const cardShape = {
+                label: true,
+                description: true,
+                progress: true,
+                start_date: true,
+                end_date: true,
+                users: {
+                    show: true,
+                    values: users,
+                },
+                priority: {
+                    show: true,
+                    values: [
+                        { id: 1, color: "#FF5252", label: "high" },
+                        { id: 2, color: "#FFC975", label: "medium" },
+                        { id: 3, color: "#65D3B3", label: "low" },
+                        { id: 3, color: "#65D3B3", label: "test" },
+                    ],
+                },
+                color: true,
+                menu: true,
+                cover: true,
+                attached: false,
+            };
+
+            const editorShape = [
+                {
+                    type: "multiselect",
+                    key: "users",
+                    label: "Users",
+                    options: users,
+                },
+            ];
+
+            const columns = [
+                {
+                    label: "Backlog",
+                    id: "backlog",
+                },
+                {
+                    label: "In progress",
+                    id: "inprogress",
+                },
+                {
+                    label: "Testing",
+                    id: "testing",
+                },
+                {
+                    label: "Done",
+                    id: "done",
+                },
+            ];
+
+            const rows = [
+                {
+                    label: "Feature",
+                    id: "feature",
+                },
+                {
+                    label: "Task",
+                    id: "task",
+                },
+            ];
+
+            const cards = [
+                {
+                    id: 1,
+                    label: "Integration with Angular/React",
+                    priority: 1,
+                    color: "#65D3B3",
+                    start_date: new Date("01/07/2021"),
+                    users: [3, 2],
+                    column: "backlog",
+                    type: "feature",
+                },
+                {
+                    label: "Archive the cards/boards ",
+                    priority: 3,
+                    color: "#58C3FE",
+                    users: [4],
+                    progress: 1,
+                    column: "backlog",
+                    type: "feature",
+                },
+                {
+                    label: "Searching and filtering",
+                    priority: 1,
+                    color: "#58C3FE",
+                    start_date: new Date("01/09/2021"),
+                    users: [3, 1],
+                    progress: 1,
+                    column: "backlog",
+                    type: "task",
+                },
+                {
+                    label: "Set the tasks priorities",
+                    color: "#FFC975",
+                    start_date: new Date("12/21/2020"),
+                    users: [4],
+                    progress: 75,
+                    column: "inprogress",
+                    type: "feature",
+                    attached: [
+                        {
+                            isCover: true,
+                            coverURL: "https://via.placeholder.com/630x360.png",    //  coverURL: "../assets/img-1.jpg",
+                            previewURL: "https://via.placeholder.com/630x360.png",  //  previewURL: "../assets/img-1.jpg",
+                            url: "https://via.placeholder.com/630x360.png",         //  url: "../assets/img-1.jpg",
+                            name: "630x360.png",
+                        },
+                    ],
+                },
+                {
+                    label: "Custom icons",
+                    color: "#65D3B3",
+                    start_date: new Date("01/07/2021"),
+                    users: [3, 2],
+                    column: "inprogress",
+                    type: "task",
+                },
+                {
+                    label: "Integration with Gantt",
+                    color: "#FFC975",
+                    start_date: new Date("12/21/2020"),
+                    users: [4],
+                    progress: 75,
+                    column: "inprogress",
+                    type: "task",
+                },
+                {
+                    label: "Drag and drop",
+                    priority: 1,
+                    color: "#58C3FE",
+                    users: [3, 1],
+                    progress: 100,
+                    column: "testing",
+                    type: "feature",
+                },
+                {
+                    label: "Adding images",
+                    color: "#58C3FE",
+                    users: [4],
+                    column: "testing",
+                    type: "task",
+                    attached: [
+                        {
+                            isCover: true,
+                            coverURL: "https://via.placeholder.com/630x360.png",
+                            previewURL: "https://via.placeholder.com/630x360.png",
+                            url: "https://via.placeholder.com/630x360.png",
+                            name: "img-2.jpg",
+                        },
+                    ],
+                },
+                {
+                    label: "Create cards and lists from the UI and from code",
+                    priority: 3,
+                    color: "#65D3B3",
+                    start_date: new Date("01/07/2021"),
+                    users: [3, 2],
+                    column: "done",
+                    type: "feature",
+                },
+                {
+                    id: 5,
+                    label: "Draw swimlanes",
+                    color: "#FFC975",
+                    users: [2],
+                    column: "done",
+                    type: "feature",
+                },
+                {
+                    label: "Progress bar",
+                    priority: 1,
+                    color: "#FFC975",
+                    start_date: new Date("12/9/2020"),
+                    users: [1, 4, 3],
+                    progress: 100,
+                    column: "done",
+                    type: "task",
+                },
+            ];
+
+            return {
+                rows,
+                columns,
+                cards,
+                users,
+                cardShape,
+                editorShape,
+            };
         }
+        const { Kanban, Toolbar, defaultEditorShape } = kanban;
+        const { columns, cards, rows, users, cardShape, editorShape } = getData();
 
-        const allowDrop = (ev) => {
-            ev.preventDefault();
-            if (hasClass(ev.target,"dropzone")) {
-                addClass(ev.target,"droppable");
-            }
-        }
-
-        const clearDrop = (ev) => {
-            removeClass(ev.target,"droppable");
-        }
-
-        const drop = (event) => {
-            event.preventDefault();
-            const data = event.dataTransfer.getData("text/plain");
-            const element = document.querySelector(`#${data}`);
-            try {
-                // remove the spacer content from dropzone
-                //event.target.removeChild(event.target.firstChild);
-                // add the draggable content
-                event.target.appendChild(element);
-                // remove the dropzone parent
-                unwrap(event.target);
-            } catch (error) {
-                console.warn("can't move the item to the same place")
-            }
-            updateDropzones();
-        }
-
-        const updateDropzones = () => {
-            /* after dropping, refresh the drop target areas
-              so there is a dropzone after each item
-              using jQuery here for simplicity */
-
-            var dz = $('<div class="dropzone rounded" ondrop="drop(event)" ondragover="allowDrop(event)" ondragleave="clearDrop(event)"></div>');
-
-            // delete old dropzones
-            $('.dropzone').remove();
-
-            // insert new dropdzone after each item
-            dz.insertBefore('.card.draggable');
-            dz.insertAfter('.card.draggable');
-
-            // insert new dropzone in any empty swimlanes
-            $(".items:not(:has(.card.draggable))").append(dz);
-        };
-
-        // helpers
-        function hasClass(target, className) {
-            return new RegExp('(\\s|^)' + className + '(\\s|$)').test(target.className);
-        }
-
-        function addClass(ele,cls) {
-            if (!hasClass(ele,cls)) ele.className += " "+cls;
-        }
-
-        function removeClass(ele,cls) {
-            if (hasClass(ele,cls)) {
-                var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
-                ele.className=ele.className.replace(reg,' ');
-            }
-        }
-
-        function unwrap(node) {
-            node.replaceWith(...node.childNodes);
-        }
+        const board = new Kanban("#root", {
+            columns,
+            cards,
+            rows,
+            rowKey: "type",
+            cardShape,
+            editorShape: [...defaultEditorShape, ...editorShape],
+        });
+        new Toolbar("#toolbar", {
+            api: board.api,
+        });
     </script>
 @endsection
